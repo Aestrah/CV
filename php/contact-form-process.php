9f18033483077843e8f2a270f4dc5e885fa53bc7
Ludo <94895249+Aestrah@users.noreply.github.com>
2022-06-08 15:26:50 +0200

    Test mailer

---
+++ php/contact-form-process.php
@@ -0,0 +1,61 @@
+<?php
+if (isset($_POST['Email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "mourier.ludovic.dev@hotmail.com";
+    $email_subject = "Stage d'entreprise";
+
+    function problem($error)
+    {
+        echo "Une erreur à eu lieu concernant l'adresse mail.";
+        echo "Cette erreur s'affiche ci-dessous. <br><br>";
+        echo $error . "<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['Email']) 
+    ) {
+        problem("Une erreur à eu lieu concernant l'adresse mail.");
+    }
+
+    $email = $_POST['Email']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Email: " . clean_string($email) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    mail($email_to, $email_subject, $headers);
+?>
+
+    <!-- include your success message below -->
+
+    "Merci de m'avoir contacté. Je prends contact avec vous dans les plus brefs délai."
+
+<?php
+}
+?>
